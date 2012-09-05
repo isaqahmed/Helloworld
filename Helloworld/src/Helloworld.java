f9a1515f6ea90caa3e5788220b0ea00ae834b4d6
isaq <isaq@outlook.com>
2012-09-05 23:17:41 +0800

    adding fabinocci

--- Helloworld/src/Helloworld.java
+++ Helloworld/src/Helloworld.java
@@ -5,6 +5,15 @@ public class Helloworld
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println(55*2);
-		
+		for(int i = 1; i < 10; i++)
+		System.out.println(fabinonci(i));
+	}
+
+	private static int fabinonci(int n)
+	{
+		if( n <= 2 )
+			return 1;
+		else
+			return fabinonci(n - 1) + fabinonci(n- 2);
 	}
 }
